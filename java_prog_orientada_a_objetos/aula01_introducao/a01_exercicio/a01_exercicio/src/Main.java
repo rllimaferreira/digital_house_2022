c70e139bdfa7edebe50c589ba0c11fd24446b8bd
Rodrigo L. de Lima Ferreira <rodrigollferreira21@gmail.com>
2022-05-16 22:36:24 -0300

    Atividades

---
+++ java_prog_orientada_a_objetos/aula01_introducao/a01_exercicio/a01_exercicio/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        String nome1 = "Shessheu", nome2 = "Nemo", nome3 = "Snow", resp1 = "au au.", resp2 = "grub grub.", resp3 = "miau miau.", qtde1 = "um quilo e meio", qtde2 = "cento e quarenta gramas", qtde3 = "meio quilo";
+        int ano1 = 2, ano2 = 1, ano3 = 3;
+
+        System.out.println(nome1 + " " + ano1 + " anos.");
+        System.out.println(nome1 +  " come " + qtde1 + " e faz " + resp1);
+        System.out.println(nome2 + " " + ano1 + " anos.");
+        System.out.println(nome2 + " come " + qtde2 + " e faz " + resp2);
+        System.out.println(nome3 + " " + ano3 + " anos.");
+        System.out.println(nome3 + " come " + qtde3 + " e faz " + resp3);
+    }
+}
+
+
